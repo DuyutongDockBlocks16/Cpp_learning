6b4a1c14c1f0752cc06179373df1a46e9c2a8caa
YutongDu <yutong.du@aalto.fi>
2024-01-17 10:22:34 +0200

    增加了person的相关代码

--- Module_2/T2_3_Simple_class/src/person.cpp
+++ Module_2/T2_3_Simple_class/src/person.cpp
@@ -1,4 +1,16 @@
 #include "person.hpp"
 
 // define your Person class' functions here
+#include <iostream>
 
+Person::Person(std::string name, int birth_year) : Name(name), birthyear(birth_year) {
+    // 在构造函数的初始化列表中初始化成员变量
+}
+
+std::string Person::GetName() {
+    return Name;
+}
+
+int Person::GetAge(int year) {
+    return year - birthyear;
+}
--- Module_2/T2_3_Simple_class/src/person.hpp
+++ Module_2/T2_3_Simple_class/src/person.hpp
@@ -1,7 +1,18 @@
 #ifndef AALTO_ELEC_CPP_PERSON
 #define AALTO_ELEC_CPP_PERSON
 
+#include <string>
+
 // declare your Person class here
+class Person {
+public:
+    std::string Name;
+    int birthyear;
+
+    Person(std::string name, int birth_year);
+    std::string GetName();
+    int GetAge(int year);
+};
 
 
 #endif
