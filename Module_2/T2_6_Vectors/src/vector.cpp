e1184e80e37a88fa9503787820b9f0863b26c355
YutongDu <yutong.du@aalto.fi>
2024-01-18 12:13:03 +0200

    modified the logic of max

--- Module_2/T2_6_Vectors/src/vector.cpp
+++ Module_2/T2_6_Vectors/src/vector.cpp
@@ -7,7 +7,7 @@ int GetMin(std::vector<int> v) {
     int min = v[0];
 
     for(unsigned i = 0; i<v.size(); i++){
-        if(v[i] < i){
+        if(v[i] < min){
             min = v[i];
         }
     }
@@ -21,7 +21,7 @@ int GetMax(std::vector<int> v) {
     int max = v[0];
 
     for(unsigned i = 0; i<v.size(); i++){
-        if(v[i] > i){
+        if(v[i] > max){
             max = v[i];
         }
     }
