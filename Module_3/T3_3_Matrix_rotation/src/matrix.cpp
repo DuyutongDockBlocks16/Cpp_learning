d19716b69f755a0c096f76292a6ec0adf1cb06c0
YutongDu <yutong.du@aalto.fi>
2024-01-23 09:44:37 +0200

    增加了T3.3的代码

--- Module_3/T3_3_Matrix_rotation/src/matrix.cpp
+++ Module_3/T3_3_Matrix_rotation/src/matrix.cpp
@@ -8,10 +8,72 @@
 
 // Assumes 'm' is a square matrix
 Matrix ReadMatrix(int n) {
-  }
+    int dimension_number = n * n;
+    std::vector<int> elements;
+    elements.clear();
+    int input_number;
+
+//    collect elements
+    for(int i = 0; i < dimension_number; i++){
+
+        std::cin >> input_number;
+
+        elements.push_back(input_number);
+    }
+
+//    test:
+//    for (int element : elements) {
+//        std::cout << element << " ";
+//    }
+//    std::cout << std::endl;
+
+//    break down elements to matrix
+    Matrix matrix;
+    auto element = elements.begin();
+    for(int i = 0; i < n; ++i){
+        std::vector<int> row;
+        for(int j=0; j < n; ++j){
+            if(element != elements.end()){
+                row.push_back(*element);
+                element++;
+            }
+        }
+        matrix.push_back(row);
+    }
+
+//    // print for test
+//    for (const auto& row : matrix) {
+//        for (int element : row) {
+//            std::cout << element << " ";
+//        }
+//        std::cout << std::endl;
+//    }
+
+    return matrix;
+
+}
 
 Matrix Rotate90Deg(const Matrix &m) {
-  }
+
+    int rows = m.size();
+    int cols = m[0].size();
+    Matrix rotated(cols, std::vector<int>(rows));
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            rotated[j][rows - 1 - i] = m[i][j];
+        }
+    }
+
+    return rotated;
+
+}
 
 void Print(const Matrix &m) {
-  }
+    for (const auto& row : m) {
+        for (int element : row) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
+}
