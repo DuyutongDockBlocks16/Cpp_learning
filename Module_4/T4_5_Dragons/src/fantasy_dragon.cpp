a1482d5898221a41dc549bdcf6fe40b1e37201b2
YutongDu <yutong.du@aalto.fi>
2024-01-30 10:51:54 +0200

    重写了hoard和eat的逻辑

--- Module_4/T4_5_Dragons/src/fantasy_dragon.cpp
+++ Module_4/T4_5_Dragons/src/fantasy_dragon.cpp
@@ -4,41 +4,26 @@
 
 
 void FantasyDragon::Eat(std::list<Food> &foodList) {
-    for (auto it = foodList.begin(); it != foodList.end();)
-    {
-        if (it->type == People || it->type == PeopleFood)
-        {
-            // 吃食物，增加大小
-            size_++;
-            // 打印被吃掉的食物信息
-            std::cout << "Fantasy dragon ate: " << it->name << "\n";
-            // 从食物列表中移除被吃掉的食物
-            it = foodList.erase(it);
+    for(auto i = foodList.begin(); i != foodList.end();){
+        if(i->type == People or i->type == PeopleFood){
+            size_ = size_ + 1;
+            std::cout << "Fantasy dragon ate: ";
+            std::cout << i->name << std::endl;
+            //delete (*i);
+            i = foodList.erase(i);
         }
-        else
-        {
-            // 不吃这种食物，继续检查下一个食物
-            ++it;
-        }
+        else i++;
     }
 }
 
 void FantasyDragon::Hoard(std::list<Treasure> &treasureList) {
-    for (auto it = treasureList.begin(); it != treasureList.end();)
-    {
-        if (it->type == Jewellery)
-        {
-            // hoard 宝藏，添加到宝藏列表
-            treasures_.push_back(*it);
-            // 打印 hoard 的宝藏信息
-            std::cout << "Fantasy dragon hoarded: " << it->name << "\n";
-            // 从传入的宝藏列表中移除 hoard 的宝藏
-            it = treasureList.erase(it);
+    for(auto i = treasureList.begin(); i != treasureList.end();){
+        if(i->type == Jewellery){
+            treasures_.push_back(*i);
+            std::cout << "Fantasy dragon received: ";
+            std::cout << i->name << std::endl;
+            i = treasureList.erase(i);
         }
-        else
-        {
-            // 不 hoard 这种宝藏，继续检查下一个宝藏
-            ++it;
-        }
+        else i++;
     }
 }
